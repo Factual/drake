2b31eca0f62b55a4337b39622e6990e4385598b8
Aaron Crow <aaron@factual.com>
2013-04-18 09:40:32 -0700

    Reset Hadoop core dep to 0.20.2

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                  ;; for HDFS support
                  [hdfs-clj "0.1.0"]
                  ;; you may need to change this to be compatible with your cluster
-                 [org.apache.hadoop/hadoop-core "0.20.2-cdh3u4"]
+                 [org.apache.hadoop/hadoop-core "0.20.2"]
                  ;; for AWS S3 support
                  [clj-aws-s3 "0.3.3"]]
   :test-selectors {:regression   :regression
