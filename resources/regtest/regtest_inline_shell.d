5eb6426c5fdd48c3c37540925bb532ceae8e187c
Myron Ahn <myron@factual.com>
2013-09-12 15:15:09 +0700

    Cleaned up inline shell integration/regression tests #63

--- resources/regtest/regtest_inline_shell.d
+++ resources/regtest/regtest_inline_shell.d
@@ -1,2 +1,2 @@
-$(for FILE in dude.txt babe.txt belle.txt; do echo "FILE=$FILE"; cat regtest_inline_shell_helper.d; done)
+$(for FILE in $FILES; do echo "FILE=$FILE"; cat regtest_inline_shell_helper.d; done)
 
--- resources/regtest/regtest_inline_shell.sh
+++ resources/regtest/regtest_inline_shell.sh
@@ -3,8 +3,13 @@
 
 source $(dirname $0)/regtest_utils.sh
 
-run() {
-  run_d regtest_methods.d -a $@
+export FILES=dude.txt\ babe.txt\ belle.txt\ bogus.txt
+
+cleanup() {
+  for FILE in $FILES
+  do
+    rm -f $(dirname $0)/$FILE>/dev/null 2>&1
+  done
 }
 
 echo "-----------"
@@ -12,14 +17,18 @@ echo "TESTS: loop"
 echo "-----------"
 
 # First cleanup any existing files
-rm -f $(dirname $0)/dude.txt>/dev/null 2>&1
-rm -f $(dirname $0)/babe.txt>/dev/null 2>&1
-rm -f $(dirname $0)/belle.txt>/dev/null 2>&1
+cleanup
 
+# Run loop test
 run_d regtest_inline_shell.d -a
 
-check_grep $(dirname $0)/dude.txt "dude.txt"
-check_grep $(dirname $0)/babe.txt "babe.txt"
-check_grep $(dirname $0)/belle.txt "belle.txt"
-
+# Check all results
+for FILE in $FILES
+do
+check_grep $(dirname $0)/$FILE "$FILE"
+done
 echo "ALL PASSED"
+
+# Clean up again
+cleanup
+
