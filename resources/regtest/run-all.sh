6799321cb7c81f22fccbd610fbe85ecf5cc7e374
Myron Ahn <myron@factual.com>
2013-11-05 16:16:04 +0700

    Added checkpoints testing to run-all regtest script #88

--- resources/regtest/run-all.sh
+++ resources/regtest/run-all.sh
@@ -8,6 +8,7 @@ if ($(dirname $0)/regtest_fs.sh &&
     $(dirname $0)/regtest_inputs_outputs.sh &&
     $(dirname $0)/regtest_methods.sh &&
     $(dirname $0)/regtest_protocol_eval.sh &&
+    $(dirname $0)/regtest_temp.sh &&
     $(dirname $0)/regtest_s3.sh); then
   echo "run-all: ALL TESTS PASSED"
 else
