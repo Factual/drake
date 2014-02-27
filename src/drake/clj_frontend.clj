ed84664663ab687ded3a0cd7380fbd3539de2dab
Morris Feldman <morrifeldman@gmail.com>
2014-02-27 19:17:06 +0200

    Clean up code and add tests
    Move helper functions into clj-frontend-utils ns.
    
    Fix several problems in variable substitution.
    
    Add tests.

--- src/drake/clj_frontend.clj
+++ src/drake/clj_frontend.clj
@@ -1,270 +1,156 @@
 (ns drake.clj-frontend
-  (:require [drake.parser :as parse]
-            [drake.parser_utils :refer [state-s
-                                        illegal-syntax-error-fn
-                                        throw-parse-error]]
+  "Clojure frontend to drake"
+  (:require [clojure.core.memoize :as memo]
+            [clojure.tools.logging :refer [debug info warn]]
+            [drake.clj-frontend-utils :as utils]
             [drake.core :as d-core]
-            [drake.plugins :as plug]
             [drake.options :as d-opts :refer [*options*]]
-            [drake.utils :as d-utils]
-            [drake.steps :as d-steps]
-            [clojure.core.memoize :as memo]
-            [clojure.tools.logging :refer [warn debug info]]
-            [fs.core :as fs]
-            [clojure.string :as str]
-            [slingshot.slingshot :refer [throw+]]
-            [name.choi.joshua.fnparse :as p]))
+            [drake.parser :as parse]
+            [drake.plugins :as plug]
+            [fs.core :as fs]))
 
-(defn tprn
-  "Transparent prn"
-  [x]
-  (prn x)
-  x)
 
-(defn new-workflow
-  "Create a new workflow.  Optionally specify vars to overide defaults"
-  ([] (new-workflow false))
+(defn workflow
+  "Create a new workflow.  Optionally specify vars to overide defaults
+  environmental variables"
+  ([] (workflow false))
   ([vars]
      {:steps []
       :methods {}
       :vars (if vars vars (d-core/build-vars))}))
 
-(def var-re
-  "Regular Expression for variable subsitution.  re-groups on the
-  match should return a two element vector, where the first element is
-  $[XXX] and the second is just XXX"
-  #"\$\[([^\]]*)\]")
-
-(defn- var-check
-  "Check if var-name is a key in vars.  If it isn't raise an error."
-  [vars var-name]
-  (when-not (contains? vars var-name)
-    (throw+
-     {:msg (format "variable \"%s\" undefined at this point." var-name)})))
-
-;; (var-check {"a" 1 "b" 2} "a")
-;; (var-check {"a" 1 "b" 2} "c")
-
-(defn- var-sub
-  "Substitute the matches to $[XXX] with the value of XXX in the vars
-  map. Throw an error if XXX is not found.  Return an array of characters"
-  [vars s]
-  (let [sub-fn (fn [var-match]
-                 (let [var-name (second var-match)]
-                   (var-check vars var-name)
-                   (vars var-name)))]
-    (concat (str/replace s var-re sub-fn))))
-
-(defn- var-sub->str
-  [vars s]
-  (apply str (var-sub vars s)))
-
-;; (var-sub {"xxx" "value1" "yyy" "value2"} "test$[xxx]sdf $[yyy] sdf")
-;; (var-sub {"xxx" "value1"} "test$[xxx]sdf $[yyy] sdf")
-;; (var-sub {} "test no var")
-
-(defn- var-place
-  "Replace all $[XXX] in a string with #{XXX}. Return an array of
-  characters and #{XXX} placeholders.  To check that the var-names
-  exist, pass in a vars map."
-  ([s]
-     (var-place false s))
-  ([vars s]
-     (let [without-vars (map concat (str/split s var-re))
-           var-names (map second (re-seq var-re s))
-           placeholders (map hash-set var-names)]
-       (when vars (dorun (map (partial var-check vars) var-names)))
-       (if-not (empty? var-names)
-         (flatten (interleave without-vars placeholders))
-         (concat s)))))
-
-;; (var-place "test$[xxx]sdf $[yyy] sdf")
-;; (var-place {"xxx" 1 "yyy" 2} "test$[xxx]sdf $[yyy] dfd" )
-;; (var-place {"xxx" 1} "test$[xxx]sdf $[yyy] dfd")
-;; (var-place "test$OUTPUT")
-;; (var-place {"xxx" 1} "test$OUTPUT")
-;; (map (partial var-place {"xxx" 1 "yyy" 2}) ["test$[xxx]sdf $[yyy] dfd"])
-
-(defn add-placeholder-vars
-  [vars infiles outfiles]
-  (merge vars
-         (into {}
-               (map #(vector (first %1) "*placeholder*")
-                    (merge (parse/inouts-map infiles
-                                             "INPUT")
-                           (parse/inouts-map outfiles
-                                             "OUTPUT"))))))
-
-(defn remove-prefix
-  "Remove ! and ? prefixes from the file f"
-  [f]
-  (if (#{\! \?} (first f))
-    (d-utils/clip f)
-    f))
-
-;; To Do.  These will all need tests.
-(defn check-step-validity
-  "Make sure a step is valid.  Possible problems: Undefined method,
-  invalid method-mode, method-mode is set without method, commands
-  with method-mode set to \"use\""
-  [p-tree step-map]
-  (let [step-method (get-in step-map [:opts :method])
-        method-mode (get-in step-map [:opts :method-mode])
-        methods (set (keys (:methods p-tree)))
-        commands (:cmds step-map)
-        state nil]
-    (cond
-     (not (or (empty? step-method) (methods step-method)))
-     (throw-parse-error state
-                        "method '%s' undefined at this point."
-                        step-method)
-
-     (not (or (empty? method-mode) (#{"use" "append" "replace"} method-mode)))
-     (throw-parse-error state
-                        (str "%s is not a valid method-mode, valid values are: "
-                             "use (default), append, and replace.")
-                        method-mode)
-
-     (not (or step-method (empty? method-mode)))
-     (throw-parse-error state
-                        "method-mode specified but method name not given"
-                        nil)
-
-     (and step-method (not (#{"append" "replace"} method-mode))
-          (not (empty? commands)))
-     (throw-parse-error state
-                        (str "commands not allowed for method calls "
-                             "(use method-mode:append or method-mode:replace "
-                             "to allow)")
-                        nil))))
-
 (defn step
-  "Add a new step to the parse tree, p-tree.  infiles and outfiles are
-  vectors of filenames.  input-tags and output-tags are optionally
-  vectors of tag strings without the opening %. To specify commands
-  use the :cmds option.  :options is an optional map of options.
-  :method and :method-mode can be an option in the options map.  See
-  add-method-step and add-cmd-step"
-  [p-tree outfiles infiles & {:keys [input-tags
-                                     output-tags
-                                     cmds
-                                     options]}]
-  (let [vars (merge (:vars p-tree))
+  "Add a new step parse-tree.  inputs and outputs are vectors and can
+  be a mixture of tags and files. Tags are indicated by an opening
+  %. cmds should be a vector of strings or nil/false for steps that
+  don't need commands like method and template steps. options are
+  standard drake options as key value pairs, e.g. :method-mode true.
+  See method-step, cmd-step, template and template-step."
+  [parse-tree outputs inputs cmds & {:keys [template]
+                                 :as options}]
+  (let [vars (:vars parse-tree)
         base (parse/add-path-sep-suffix
               (get vars "BASE" parse/default-base))
         parse-file-fn (comp
                        (partial parse/add-prefix base)
-                       (partial var-sub->str vars))
-        infiles-with-base (map parse-file-fn infiles)
-        outfiles-with-base (map parse-file-fn outfiles)
+                       (partial utils/var-sub->str vars))
+
+        [intags infiles] (utils/split-tags-from-files inputs)
+        intags (utils/remove-tag-symbol intags)
+        sub-infiles (map (partial utils/var-sub->str vars) infiles)
+        infiles-with-base (map (partial parse/add-prefix base) sub-infiles)
+
+        [outtags outfiles] (utils/split-tags-from-files outputs)
+        outtags (utils/remove-tag-symbol outtags)
+        sub-outfiles (map (partial utils/var-sub->str vars) outfiles)
+        outfiles-with-base (map (partial parse/add-prefix base) sub-outfiles)
+
         ;; this is used for target matching, just remove all
         ;; prefixes
-        outfiles-raw (mapv remove-prefix outfiles)
+        outfiles-raw (mapv utils/remove-prefix sub-outfiles)
+
         ;; even though we will expand INPUT and OUTPUT variables later,
         ;; for now just put placeholders there for variable name checking
-        vars (add-placeholder-vars vars infiles-with-base outfiles-with-base)
+        vars (utils/add-placeholder-vars
+              vars
+              infiles-with-base
+              outfiles-with-base)
+
         step-map {:inputs      infiles-with-base
-                  :input-tags  (if (nil? input-tags) () input-tags)
+                  :input-tags  (if (nil? intags) () intags)
                   :raw-outputs outfiles-raw
                   :outputs     outfiles-with-base
-                  :output-tags (if (nil? output-tags) () output-tags)
+                  :output-tags (if (nil? outtags) () outtags)
                   :vars        vars
                   :opts        (if (nil? options) {} options)
                   :cmds        (when cmds
-                                 (mapv (partial var-place vars) cmds))}]
-    (check-step-validity p-tree step-map)
-    (update-in p-tree [:steps] #(conj % step-map))))
+                                 (mapv (partial utils/var-place vars) cmds))}
+        p-tree-key (if template :templates :steps)]
+    (utils/check-step-validity parse-tree step-map)
+    (update-in parse-tree [p-tree-key]
+               #(if % (conj % step-map) [step-map]))))
 
 (defn method-step
-  "Shortcut for making a step using a method.  infiles, outfiles,
-  input-tags and output-tags should be vectors.  method is a string
-  with the method name."
-  [p-tree outfiles infiles method & {:keys [input-tags
-                                            output-tags
-                                            options]}]
-  (step p-tree outfiles infiles
-        :input-tags input-tags
-        :output-tags output-tags
-        :options (assoc options :method method)))
+  "Shortcut for adding a step using a method to parse-tree. method
+  name should be string.  method-step does not support method-steps
+  with commands in conjunction with :method-mode \"append\" or
+  \"replace\".  For a method-step with commands use step and specify
+  :method and :method-mode options. See step for outputs inputs and
+  options."
+  [parse-tree outputs inputs method-name & options]
+  (apply step parse-tree outputs inputs nil :method method-name options))
 
-(defn cmd-step
-  "Shortcut for making a step with commands. infiles, outfiles and
-  cmds should be vectors.  Optionallly, input-tags and output-tags
-  should contain vectors of tags without an opening %."
-  [p-tree outfiles infiles cmds & {:keys [input-tags
-                                          output-tags
-                                          options]}]
-  (step p-tree outfiles infiles
-        :cmds cmds
-        :input-tags input-tags
-        :output-tags output-tags
-        :options options))
+(def cmd-step
+  "Shortcut for adding a command step to parse-tree. See step for
+  outputs inputs cmds and options."
+  step)
+
+(defn template
+  "Shortcut for adding a template to parse-tree.  See step for
+  outputs, inputs, cmds and options."
+  [parse-tree outputs inputs cmds & options]
+  (apply step parse-tree outputs inputs cmds :template true options))
+
+(defn template-step
+  "Shortcut for adding a step that uses a template to parse-tree.  See
+  step for outputs, inputs, cmds and options"
+  [parse-tree outputs inputs & options]
+  (apply step parse-tree outputs inputs nil options))
 
 (defn method
-  "Add a method to the parse tree.  method-name should be a string and
-  cmds should be a vector of command strings"
-  [p-tree method-name cmds & {:keys [options]
-                              :or {options {}}}]
-  (when ((:methods p-tree) method-name)
-    (warn (format "Warning: method redefinition ('%s')" method-name)))
-  (assoc-in p-tree [:methods method-name] {:options options
-                                           :vars (:vars p-tree)
-                                           :cmds (mapv var-place cmds)}))
+  "Add a method to parse-tree.  method-name should be a string and cmds
+  should be a vector of command strings.  Options are standard drake
+  options as key value pairs, e.g. :my-option \"my-value\""
+  [parse-tree method-name cmds & {:as options}]
+  (when ((:methods parse-tree) method-name)
+    (println (format "Warning: method redefinition ('%s')" method-name)))
+  (assoc-in parse-tree [:methods method-name] {:options (if (nil? options)
+                                                      {}
+                                                      options)
+                                           :vars (:vars parse-tree)
+                                           :cmds (mapv utils/var-place cmds)}))
 
 (defn add-methods
-  "Adds all the methods in methods-hash to the p-tree tree.
-  methods-hash has method names for keys and vectors of method
-  commands for values like this {\"method-name\" [\"method commands\"]}"
-  [p-tree methods-hash]
+  "Adds all the methods in methods-hash to parse-tree.  methods-hash
+  has method names for keys and vectors of method commands for values
+  like this {\"method-name\" [\"method commands\"]}"
+  [parse-tree methods-hash]
   (reduce
-   (fn [p-tree [method-name cmds]]
-     (method p-tree method-name cmds))
-   p-tree
+   (fn [parse-tree [method-name cmds]]
+     (method parse-tree method-name cmds))
+   parse-tree
    (seq methods-hash)))
 
 (defn set-var
-  "Add a variable to the parse tree.  var-name and var-value should
+  "Add a variable to parse-tree.  var-name and var-value should
   be strings"
-  [p-tree var-name var-value]
-  (let [vars (:vars p-tree)
-        sub-var-name (var-sub->str vars var-name)
-        sub-var-value (var-sub->str vars var-value)]
-    (assoc-in p-tree [:vars sub-var-name] sub-var-value)))
+  [parse-tree var-name var-value]
+  (let [vars (:vars parse-tree)
+        sub-var-name (utils/var-sub->str vars var-name)
+        sub-var-value (utils/var-sub->str vars var-value)]
+    (assoc-in parse-tree [:vars sub-var-name] sub-var-value)))
 
 (defn base
   "Shortcut to set BASE to new-base"
-  [p-tree new-base]
-  (set-var p-tree "BASE" new-base))
+  [parse-tree new-base]
+  (set-var parse-tree "BASE" new-base))
 
-(defn- add-step-ids
-  "Add Unique ID to each step in parse-tree"
-  [parse-tree]
-  (let [steps (map (fn [step]
-                     (assoc step :id (str (java.util.UUID/randomUUID))))
-                   (:steps parse-tree)) ; add unique ID to each step
-        steps (into [] steps)]
-    (assoc parse-tree :steps steps)))
 
-(defn- compile-parse-tree
-  "add-dependencies, calc-step-dirs and add-step-ids to
-  p-tree"
-  [p-tree]
-  (-> p-tree
-      d-steps/add-dependencies
-      d-steps/calc-step-dirs
-      add-step-ids))
-
+;; This function should print some interactive output to the repl as
+;; steps are run.  Currently it just blocks with no output till all
+;; the steps are done.  I'm not quite sure how to do this.  Can it be
+;; through the logging configuration?
 (defn run-workflow
-  "Run the workflow in p-tree.  Optionally specify :targetv, otherwise
-  defaults to [\"=...\"]\".  Other options to run-workflow can be
-  specified as a map to :opts"
-  [p-tree & {:keys [targetv]
+  "Run the workflow in parse-tree.  Optionally specify targetv as a
+  key value pair, e.g. :targetv [\"=...\"]\", otherwise the default
+  targetv is [\"=...\"]\".  Other run options to run-workflow can also
+  be specified as key value pairs"
+  [parse-tree & {:keys [targetv]
               :or {targetv ["=..."]}
               :as run-options}]
   (let [opts (merge d-core/DEFAULT-OPTIONS run-options)]
     (d-opts/set-options opts)
-    (d-core/configure-logging)          ; currently configure-logging
-                                        ; is private
+    (d-core/configure-logging)
     (memo/memo-clear! parse/shell-memo)
 
     (debug "Drake" d-core/VERSION)
@@ -273,111 +159,38 @@
 
     (plug/load-plugin-deps (*options* :plugins))
     (fs/with-cwd fs/*cwd*
-      (-> p-tree
-          (compile-parse-tree)
+      (-> parse-tree
+          (utils/compile-parse-tree)
           (d-core/run targetv)))))
 
-;; ;; Example usage:
+
+;; Example usage:
+
 ;; (use 'drake.clj-frontend)
 
-(def p-tree (-> (new-workflow {})
-                (cmd-step
-                 ["out1"]
-                 []
-                 ["echo \"This is the first output.\" > $OUTPUT"]
-                 :options {:timecheck false})
-                (method
-                 "test_method"
-                 ["echo \"Here we are testing a method step.\" > $OUTPUT"])
-                (method-step
-                 ["out_method"]
-                 []
-                 "test_method")
-                (set-var "test_var" "TEST_VAR_VALUE")
-                (set-var "output_three" "out3")
-                (cmd-step
-                 ["$[output_three]"]
-                 ["out1"]
-                 ["echo \\
-\"This is the third output.
-test_var is set to $test_var and $[test_var].
-The file $INPUT contains:\" \\
-| cat - $INPUT > $[OUTPUT]"])
-                ))
+(def p-tree
+  (->
+   (workflow {})
+   (cmd-step
+    ["out1"]
+    []
+    ["echo \"This is the first output.\" > $OUTPUT"]
+    :timecheck false)
+   (method
+    "test_method"
+    ["echo \"Here we are testing a method step.\" > $OUTPUT"])
+   (method-step
+    ["out_method"]
+    []
+    "test_method")
+   (set-var "test_var" "TEST_VAR_VALUE")
+   (set-var "output_three" "out3")
+   (cmd-step
+    ["$[output_three]"]
+    ["out1"]
+    ["echo \"This is the third output.\" > $OUTPUT"
+     "echo \"test_var is set to $test_var - $[test_var].\" >> $OUTPUT"
+     "echo \"The file $INPUT contains:\" | cat - $INPUT >> $[OUTPUT]"])
+   ))
 
 ;; (run-workflow p-tree)
-
-;; Things I need to test
-
-;; INPUTS and multiple inputs
-;; outputs and multiple outputs
-;; changing directory, try setting BASE as a variable
-;; Variables in general
-;; Options: -timescheck, protocol, at least do a simple ruby protocol
-;; targetv
-;; methods with different method-modes
-;; various run-options, :opts, to run-workflow
-(defn- ensure-final-newline
-  "Make sure the string ends with a newline"
-  [s]
-  (if (.endsWith s "\n")
-    s
-    (str s "\n")))
-
-(defn str->p-tree
-  "Take a string s and map vars and a make a raw p-tree."
-  ([s vars]
-     (let [state (struct state-s
-                         (ensure-final-newline s)
-                         (merge {"BASE" parse/default-base} vars)
-                         #{}
-                         1 1)]
-       (p/rule-match parse/workflow
-                     #((illegal-syntax-error-fn "start of workflow")
-                       (:remainder %2) %2) ;; fail
-                     #((illegal-syntax-error-fn "workflow")
-                       (:remainder %2) %2) ;; incomplete match
-                     state))))
-
-
-(defn- file->p-tree
-  "Take a file and convert it to a raw parse tree for comparison with
-  results from the clojure frontend"
-  ([file-name] (file->p-tree file-name (d-core/build-vars)))
-  ([file-name vars]
-     (let [d-file (slurp file-name)]
-       (str->p-tree d-file vars))))
-
-;; (def tree (file->p-tree "test.drake.txt" {}))
-
-(defn cmd-ws-remover
-  "Simplified version of drake.core/despace-cmds, just for testing"
-  [cmds]
-  (when cmds
-    (let [prefix-len (count (take-while #{\space \tab} (first cmds)))]
-      (map #(drop prefix-len %) cmds))))
-
-(defn step-ws-remover
-  "Remove whitespace from commands in step-map"
-  [step-map]
-  (update-in step-map [:cmds] cmd-ws-remover))
-
-(defn remove-step-ws [p-tree]
-  (update-in p-tree [:steps]
-             (fn [steps]
-               (for [step-map steps]
-                 (step-ws-remover step-map)))))
-
-(defn remove-method-ws [p-tree]
-  (let [method-names (keys (:methods p-tree))]
-    (reduce (fn [p-tree meth]
-              (update-in p-tree [:methods meth]
-                         step-ws-remover))
-            p-tree
-            method-names)))
-
-(defn remove-p-tree-ws
-  [p-tree]
-  (-> p-tree
-      remove-step-ws
-      remove-method-ws))
--- test/drake/test/clj_frontend.clj
+++ test/drake/test/clj_frontend.clj
@@ -1,8 +1,250 @@
 (ns drake.test.clj-frontend
-  (:use [clojure.tools.logging :only [warn debug trace]]
-        clojure.test)
-  (:require [drake.clj-frontend :as c-f]))
+  (:require [clojure.test :refer :all]
+            [drake.clj-frontend :refer :all]
+            [drake.clj-frontend-utils :as utils]
+            [drake.parser :as parse]
+            [drake.parser_utils :refer [illegal-syntax-error-fn
+                                        state-s]]
+            [name.choi.joshua.fnparse :as p]))
+
+;; Some functions to help testing
+
+(defn ensure-final-newline
+  "Make a the string ends with a newline"
+  [s]
+  (if (.endsWith s "\n")
+    s
+    (str s "\n")))
+
+(defn step-ws-remover-fn
+  "Remove whitespace from commands in step-map"
+  [step-map]
+  (let [cmd-ws-remover-fn (fn [cmds]
+                            (when cmds
+                              (let [prefix-len (count
+                                                (take-while #{\space \tab}
+                                                            (first cmds)))]
+                                (mapv #(drop prefix-len %) cmds))))]
+    (update-in step-map [:cmds] cmd-ws-remover-fn)))
+
+(defn remove-step-template-ws
+  "Remove whitespace from all step or template commands in
+  parse-tree. parse-tree-key can be either :steps or :templates"
+  [parse-tree parse-tree-key]
+  (if (parse-tree-key parse-tree)
+    (update-in parse-tree [parse-tree-key]
+               (fn [steps]
+                 (vec
+                  (for [step-map steps]
+                    (step-ws-remover-fn step-map)))))
+    parse-tree))
+
+(defn remove-method-ws
+  "Remove whitespace from all method commands in parse-tree"
+  [parse-tree]
+  (let [method-names (keys (:methods parse-tree))]
+    (reduce (fn [parse-tree meth]
+              (update-in parse-tree [:methods meth]
+                         step-ws-remover-fn))
+            parse-tree
+            method-names)))
+
+(defn remove-parse-tree-ws
+  "Remove whitespace from step and method commands"
+  [parse-tree]
+  (-> parse-tree
+      (remove-step-template-ws :steps)
+      (remove-step-template-ws :templates)
+      remove-method-ws))
+
+(defn str->parse-tree
+  "Take a string s and the map vars and a make a raw
+  parse-tree. Remove whitespace from commands"
+  ([s]
+     (let [state (struct state-s
+                         (ensure-final-newline s)
+                         {}
+                         #{}
+                         1 1)]
+       (remove-parse-tree-ws
+        (p/rule-match parse/workflow
+                      #((illegal-syntax-error-fn "start of workflow")
+                        (:remainder %2) %2) ;; fail
+                      #((illegal-syntax-error-fn "workflow")
+                        (:remainder %2) %2) ;; incomplete match
+                      state)))))
+
+
+(defn file->parse-tree
+  "Just a function to help during development.  Take a file and
+  converts it into a raw parse-tree"
+  ([file-name]
+     (let [d-file (slurp file-name)]
+       (str->parse-tree d-file))))
+
+;; (def tree (file->parse-tree "test.drake.txt" {}))
+;; (pprint tree)
+
+;; Start of the actual tests
+
+(deftest var-check-test
+  (is (= (utils/var-check {"a" 1 "b" 2} "a") nil))
+  (is (thrown? Exception
+               (utils/var-check {"a" 1 "b" 2} "c"))))
 
 (deftest var-re-test
-  (is (= (re-seq c-f/var-re "test$[xxx]sdfdsf")
-         (["$[xxx]" "xxx"]))))
+  (are [s] (= (re-find utils/var-re s) ["$[var]" "var"])
+       "$[var]"
+       "aaa$[var]"
+       "aaa$[var]bbb"
+       "$other_var $[var]"
+       "$[var] $other_var"))
+
+(deftest var-split-re-test
+  (are [s m] (= (re-find utils/var-split-re s) m)
+       "no-var" nil
+       "$no_sub" nil
+       "$[bare_var]" ["$[bare_var]" "" "bare_var" ""]
+       "a$[var]" ["a$[var]" "a" "var" ""]
+       "$[var]b" ["$[var]b" "" "var" "b"]
+       "a$[var]b" ["a$[var]b" "a" "var" "b"]
+       "a$[var1]b$[var2]" ["a$[var1]b" "a" "var1" "b"]
+       "a$[var1]b$var2" ["a$[var1]b$var2" "a" "var1" "b$var2"]))
+
+(deftest var-place-test
+  (is (= (utils/var-place "test") '(\t \e \s \t)))
+  (is (= (utils/var-place "aaa$[var1]") '(\a \a \a #{"var1"})))
+  (is (= (utils/var-place "aaa$[var1]bbb$OUTPUT")
+         '(\a \a \a #{"var1"} \b \b \b \$ \O \U \T \P \U \T)))
+  (is (= (utils/var-place "test$OUTPUT")
+         '(\t \e \s \t \$ \O \U \T \P \U \T)))
+  (is (= (utils/var-place {"var" "var-val"}
+                          "aaa$[var]")
+         '(\a \a \a #{"var"})))
+  (is (thrown? Exception
+               (utils/var-place {"not-var" "val"}
+                                "aaa$[var]"))))
+
+(deftest check-step-validity-test
+  (is (thrown-with-msg?
+       Exception
+       #"method 'future' undefined at this point."
+       (-> (workflow {})
+           (method-step ["A"] ["B"] "future")
+           (method "future" ["cmd"]))))
+  (is (thrown-with-msg?
+    Exception
+    #"bad-method-mode is not a valid method-mode, valid values are: .*"
+    (-> (workflow {})
+        (method "a-method" ["cmd"])
+        (method-step ["out"] ["in"] "a-method"
+                     :method-mode "bad-method-mode"))))
+  (is (thrown-with-msg?
+       Exception
+       #"method-mode specified but method name not given"
+       (-> (workflow {})
+           (step [] [] [] :method-mode "append")
+           )))
+  (is (thrown-with-msg?
+       Exception
+       #"commands not allowed for method calls .*"
+       (-> (workflow {})
+           (method "test-method" ["cmd"])
+           (step [] [] ["bad-commands"] :method "test-method")))))
+
+(deftest basic-wf-test
+  (is (=
+       (-> (workflow {})
+           (cmd-step ["out"] ["in"] ["cat $[INPUT] > $OUTPUT"]))
+
+       (str->parse-tree "
+out <- in
+  cat $[INPUT] > $OUTPUT"))))
+
+(deftest subsitution-in-file-test
+  (is (=
+       (-> (workflow {})
+           (set-var "var" "val")
+           (cmd-step ["$[var]"] [] ["cmd"]))
+       (str->parse-tree "
+var=val
+$[var] <-
+  cmd"))))
+
+(deftest two-commands-test
+  (is (=
+       (-> (workflow {})
+             (cmd-step ["out"] ["in"]
+                       ["echo \"first command\""
+                        "echo \"second command\""]))
+
+       (str->parse-tree "
+out <- in
+  echo \"first command\"
+  echo \"second command\""))))
+
+(deftest template-test
+  (is (=
+       (-> (workflow {})
+           (template [".sorted$"] ["."]
+                     ["cat $INPUT | sort | uniq > $OUTPUT"])
+           (template-step ["output1.sorted"] ["input1"]))
+
+       (str->parse-tree "
+.sorted$ <- . [+template]
+    cat $INPUT | sort | uniq > $OUTPUT
+output1.sorted <- input1"))))
+
+(deftest tag-test
+  (is (=
+       (-> (workflow {})
+           (cmd-step ["%A"] [] ["echo Step A"])
+           (cmd-step ["%B"] [] ["echo Step B"])
+           (cmd-step ["%C"] ["%A" "%B"] ["echo Step C"])
+           (cmd-step ["%D"] ["%C"] ["echo Step D"]))
+
+       (str->parse-tree "
+%A <-
+  echo Step A
+
+%B <-
+  echo Step B
+
+%C <- %A, %B
+  echo Step C
+
+%D <- %C
+  echo Step D"))))
+
+(deftest base-in-out-test
+  (is (=
+       (-> (workflow {})
+           (base "/tmp")
+           (cmd-step
+            ["words" "lines"]
+            ["cpg.csv"]
+            ["echo INPUTS=$[INPUTS]"
+             "echo INPUTN=$[INPUTN]"
+             "echo INPUT=$[INPUT]"
+             "echo INPUT0=$[INPUT0]"
+             "echo OUTPUTS=$[OUTPUTS]"
+             "echo OUTPUTN=$[OUTPUTN]"
+             "echo OUTPUT=$[OUTPUT]"
+             "echo OUTPUT0=$[OUTPUT0]"
+             "echo OUTPUT1=$[OUTPUT1]"]))
+       (str->parse-tree "
+BASE=/tmp
+
+words, lines <- cpg.csv
+  echo INPUTS=$[INPUTS]
+  echo INPUTN=$[INPUTN]
+  echo INPUT=$[INPUT]
+  echo INPUT0=$[INPUT0]
+  echo OUTPUTS=$[OUTPUTS]
+  echo OUTPUTN=$[OUTPUTN]
+  echo OUTPUT=$[OUTPUT]
+  echo OUTPUT0=$[OUTPUT0]
+  echo OUTPUT1=$[OUTPUT1]"))))
+
+;; add base test!!!
+;; (run-tests)
