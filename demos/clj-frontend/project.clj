7d2ac9ee315c5dbf5d3e21253de9ff6dc46d8ac0
Morris Feldman <morrifeldman@gmail.com>
2014-03-24 21:32:59 +0200

    Add demos/clj-frontend and change some names
    In addition to adding a demo, this commit also modifies clj-frontend by
    changing the workflow function to new-workflow and changing the function
    input parse-tree to w-flow.  Changing parse-tree to w-flow seems to
    better hide the implementation details of a workflow from users.

---
+++ demos/clj-frontend/project.clj
@@ -0,0 +1,8 @@
+(defproject clj-frontend "0.1.0-SNAPSHOT"
+  :description "Demo project for drake.clj-frontend"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [factual/drake "0.1.5"]]
+  :repl-options {:init-ns clj-frontend.demo})
